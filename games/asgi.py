cfff374d8aa4584ead7e69e69d0d64114c311f4c
Sai Sunku <sai2020@gmail.com>
2020-08-11 17:55:01 -0400

    new asgi.py

--- games/asgi.py
+++ games/asgi.py
@@ -1,16 +1,31 @@
+# """
+# ASGI config for games project.
+
+# It exposes the ASGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+# """
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'games.settings')
+
+# application = get_asgi_application()
+
+
 """
-ASGI config for games project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+from Channels documentation https://channels.readthedocs.io/en/latest/deploying.html#run-protocol-servers
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
 """
 
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'games.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "games.settings")
+django.setup()
+application = get_default_application()
